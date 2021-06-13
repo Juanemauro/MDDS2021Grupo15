5e3e3afc5c099cf2937dc507762edd36aa96b0f2
AlejandroCapel <alecapel2000@gmail.com>
2021-06-13 17:42:13 -0300

    Agrego Proyecto en Java

---
+++ Sprint-1/4_Incremento/SeccionInformativa/src/Sistema/Ciudadano.java
@@ -0,0 +1,21 @@
+package Sistema;
+import java.util.*;
+
+import Filtros.Filtro;
+
+public class Ciudadano {
+	public Ciudadano(){
+		//construye ciudadano
+	}
+	public List<Material> VerMateriales(){
+		//Obtiene una lista de todos los materiales en el sistema
+		List<Material> Materiales= new ArrayList<Material>(); 
+		return Materiales;
+	}
+	public List<Material> GetMateriales(Filtro f1){
+		//utiliza el filtro para obtener una búsqueda refinada en la lista de materiales
+		List<Material> Materiales= new ArrayList<Material>(); 
+		return Materiales;
+	}
+
+}
---
+++ Sprint-1/4_Incremento/SeccionInformativa/src/Sistema/SeccionInformativa.java
@@ -0,0 +1,13 @@
+package Sistema;
+import java.util.List;
+
+public class SeccionInformativa {
+	private List<Material> Meteriales;
+	
+	public void seccionInformativa(){
+		//Inicializa la seccion informativa, parametros de inicializacion y seteo de variables
+	}
+	public void AgregarMateriales(Material m1){
+		//agrega un material a la lista de materiales
+	}
+}
